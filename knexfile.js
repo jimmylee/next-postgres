01d6562312283f299f623d10014bfa7907a1f0e0
jasonleyser <jason.leyser@protocol.ai>
2020-11-11 16:10:02 -0700

    Knex config update

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 /* prettier-ignore */
 module.exports = {
   development: {
@@ -13,11 +17,11 @@ module.exports = {
   production: {
     client: 'pg',
     connection: {
-      port: 1334,
-      host: '127.0.0.1',
-      database: 'nptdb',
-      user: 'admin',
-      password: 'oblivion'
+      port: process.env.PORT,
+      host: process.env.HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS
     }
   }
 };
