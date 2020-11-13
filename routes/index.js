8b28a15ee580a46aa8655edabf9f9bb51b6242c1
jasonleyser <jason.leyser@protocol.ai>
2020-11-12 22:43:49 -0700

    Index update

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
-import signIn from '~/routes/sign-in';
-import signInConfirm from '~/routes/sign-in-confirm';
-import signInSuccess from '~/routes/sign-in-success';
-import targetOrganization from '~/routes/target-organization';
+import signIn from "~/routes/sign-in";
+import signInConfirm from "~/routes/sign-in-confirm";
+import signInSuccess from "~/routes/sign-in-success";
+import targetOrganization from "~/routes/target-organization";
 
-import apiSignIn from '~/routes/api/sign-in';
-import apiViewerDelete from '~/routes/api/viewer-delete';
+import apiSignIn from "~/routes/api/sign-in";
+import apiViewerDelete from "~/routes/api/viewer-delete";
+import apiUploadImage from "~/routes/api/upload-image";
 
 module.exports = {
   signIn,
@@ -14,5 +15,6 @@ module.exports = {
   api: {
     viewerDelete: apiViewerDelete,
     signIn: apiSignIn,
+    uploadImage: apiUploadImage,
   },
 };
